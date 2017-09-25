b46c416f91c968abe7d8bf0188aa0b6a2611b188
Aaron Samuel <aasamuelj@gmail.com>
2017-09-25 09:15:30 +0000

    automatically backed up by learn

--- ex.rb
+++ ex.rb
@@ -1,5 +1,5 @@
 cats_and_dogs = ["cat", "cat", "dog", "cat", "dog", "dog"]
-a = cats_and_dogs.reject do |item|
+a = cats_and_dogs.delete_if do |item| //reject works too
   item == "dog"
 end
 puts a.inspect
